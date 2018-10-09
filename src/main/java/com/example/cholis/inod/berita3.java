9a78793e2287a17e81312dedfd8a0e806cd4933f
DianAkhmad <43537892+DianAkhmad@users.noreply.github.com>
2018-10-09 21:37:47 +0700

    Add files via upload

---
+++ src/main/java/com/example/cholis/inod/berita3.java
@@ -0,0 +1,14 @@
+
+package com.example.cholis.inod;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class berita3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_berita3);
+    }
+}
